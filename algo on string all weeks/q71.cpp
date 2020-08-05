b6e0e4056c5ca233ee392279829ff7e8d98d6913
shivam-singh04 <68497466+shivam-singh04@users.noreply.github.com>
2020-08-06 01:35:44 +0530

    Add files via upload

---
+++ algo on string all weeks/q71.cpp
@@ -0,0 +1,62 @@
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+
+using SymbolsMap = std::unordered_map<char, int>;
+
+inline int getIndexInRrightColumn(SymbolsMap& sm, char s, int cnt)
+{
+    switch (s) {
+    case 'A':
+        return cnt;
+    case 'C':
+        return sm['A'] + cnt;
+    case 'G':
+        return sm['A'] + sm['C'] + cnt;
+    case 'T':
+        return sm['A'] + sm['C'] + sm['G'] + cnt;
+    default:
+        return 0;
+    }
+}
+
+std::string inverseBWT(const std::string& bwt)
+{
+    if (bwt.size() == 1) {
+        return bwt;
+    }
+
+    SymbolsMap symbolsMap;
+    std::vector<int> indexes;
+    indexes.reserve(bwt.size());
+
+    for (int i = 0; i < bwt.size(); ++i) {
+        indexes.emplace_back(++symbolsMap[bwt[i]]);
+    }
+
+    std::string r = "$";
+    r.reserve(bwt.size());
+
+    char s = bwt[0];
+    int j = indexes[0];
+
+    r += s;
+
+    for (int i = 2; i < bwt.size(); ++i) {
+        int indexInRrightColumn = getIndexInRrightColumn(symbolsMap, s, j);
+        r += bwt[indexInRrightColumn];
+        j = indexes[indexInRrightColumn];
+        s = r.back();
+    }
+
+    std::reverse(r.begin(), r.end());
+    return r;
+}
+
+int main()
+{
+    std::string bwt;
+    std::cin >> bwt;
+    std::cout << inverseBWT(bwt) << std::endl;
+    return 0;
+}
